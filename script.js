17b870ef76f97c81f989752defa5281fd34b6e45
szabi1112 <banki.szabolcs@gmail.com>
2022-08-09 18:13:23 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+const overLay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+const showModal = document.querySelector(".showModal");
+const escapeButton = document.querySelector(".modal__button--escape");
+const okayButton = document.querySelector(".modal__button--affirmative");
+const cancelButton = document.querySelector(".modal__button--negative");
+
+const openModal = function () {
+  overLay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  overLay.classList.add("hidden");
+  modal.classList.add("hidden");
+};
+
+showModal.addEventListener("click", openModal);
+
+escapeButton.addEventListener("click", closeModal);
+
+overLay.addEventListener("click", closeModal);
+
+okayButton.addEventListener("click", closeModal);
+
+cancelButton.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
